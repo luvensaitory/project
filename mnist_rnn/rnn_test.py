91febe2a3fe8685523c086578fd58a7177ce971c
biiii <luvensaitory@gmail.com>
2018-05-25 00:40:51 +0800

    wasting time implementing wrong idea and doing chores......QAQ

--- mnist_rnn/rnn_test.py
+++ mnist_rnn/rnn_test.py
@@ -20,25 +20,6 @@ X_test = X_test.reshape(-1, 28, 28) / 255.
 y_train = np_utils.to_categorical(y_train, num_classes=10)
 y_test = np_utils.to_categorical(y_test, num_classes=10)
 
-'''
-# 建立簡單的線性執行的模型
-model = Sequential()
-# 加 RNN 隱藏層(hidden layer)
-model.add(SimpleRNN(
-    # 如果後端使用tensorflow，batch_input_shape 的 batch_size 需設為 None.
-    # 否則執行 model.evaluate() 會有錯誤產生.
-    batch_input_shape=(None, 28, 28), 
-    units= 50,
-    unroll=True,
-)) 
-# 加 output 層
-model.add(Dense(units=10, kernel_initializer='normal', activation='softmax'))
-
-# 編譯: 選擇損失函數、優化方法及成效衡量方式
-LR = 0.001          # Learning Rate
-adam = Adam(LR)
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
-'''
 model = load_model('SimpleRNN.h5')
 model.load_weights("SimpleRNN.weight")
 img1 = Image.open('01.jpg')
@@ -48,38 +29,9 @@ img1 = img1.resize((28, 28))
 img1 = np.asarray(img1)
 img1 = img1.reshape(-1, 28, 28)/255.
 # 一批訓練多少張圖片
-'''BATCH_SIZE = 50     
-BATCH_INDEX = 0     
-# 訓練模型 4001 次
-for step in range(1, 4001):
-    # data shape = (batch_num, steps, inputs/outputs)
-    X_batch = X_train[BATCH_INDEX: BATCH_INDEX+BATCH_SIZE, :, :]
-    Y_batch = y_train[BATCH_INDEX: BATCH_INDEX+BATCH_SIZE, :]
-    # 逐批訓練
-    loss = model.train_on_batch(X_batch, Y_batch)
-    BATCH_INDEX += BATCH_SIZE
-    BATCH_INDEX = 0 if BATCH_INDEX >= X_train.shape[0] else BATCH_INDEX
 
-    # 每 500 批，顯示測試的準確率
-    if step % 500 == 0:
-        # 模型評估
-        loss, accuracy = model.evaluate(X_test, y_test, batch_size=y_test.shape[0], 
-            verbose=False)
-        print("test loss: {}  test accuracy: {}".format(loss,accuracy))
-        
-'''
 # 預測(prediction)
 #X = X_test[0:10,:]
 predictions = model.predict_classes(img1)
 # get prediction result
 print(predictions)
-'''
-# 模型結構存檔
-from keras.models import model_from_json
-json_string = model.to_json()
-with open("SimpleRNN.config", "w") as text_file:
-    text_file.write(json_string)
-    
-# 模型訓練結果存檔
-model.save_weights("SimpleRNN.weight")
-'''
--- speech_recognition/test_mfcc.py
+++ speech_recognition/test_mfcc.py
@@ -1,12 +1,19 @@
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
 import numpy as np
-
-y, sr = librosa.load("audio/mp3001/001_001.mp3")
-mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=10)
-print("mfcc:\n", mfccs.shape)
-
+from python_speech_features import mfcc
+ 
+y, sr = librosa.load("ctc/audio.wav")
+mfccs1 = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=10)
+fs, au = wav.read("ctc/audio.wav")
+mfccs2 = mfcc(au, samplerate=fs)
+print("sr=", sr, "\tfs=", fs)
+print("y=", y, "\tau=", au)
+print("mfcc1:\n", mfccs1.shape)
+print("mfcc2:\n", mfccs2.shape)
+'''
 feature_inputs = np.asarray(mfccs[np.newaxis, :])
 print("new axis:\n", feature_inputs.shape)
 
@@ -15,7 +22,7 @@ print("norm:\n", feature_inputs.shape)
 
 fl = [feature_inputs.shape[1]]
 print("le n= ", fl)
-'''
+''''''
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(mfccs, x_axis='time')
 plt.colorbar()
@@ -23,3 +30,4 @@ plt.title('MFCC')
 plt.tight_layout()
 plt.show()
 '''
+
